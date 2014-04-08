033dc3bcd762e028610c72fe224ad97b38010d06
Mark Gillard <marzer_@hotmail.com>
2014-04-09 02:44:59 +0930

    removing auto-init for testing iw command

--- wfu-setup.c
+++ wfu-setup.c
@@ -139,11 +139,19 @@ int write_rc_local(int num)
 	fprintf(file,"#!/bin/sh -e\n");
 	fprintf(file,"sleep 5\n");
 	
+	/*
 	fprintf(file,"_IS_MESH_UP=$(ip addr | grep -E -i -w \"wlan0.+state UP\") || true\n");
 	fprintf(file,"if [ \"$_IS_MESH_UP\" ]; then\n");
 	fprintf(file,"        echo \"[WFU Mesh Setup] - WFU mesh already connected\"\n");
 	fprintf(file,"else\n");
 	fprintf(file,"        echo \"[WFU Mesh Setup] - creating node...\"\n");
+	
+	fprintf(file,"        sudo iw dev wlan0 set type ibss\n");
+	fprintf(file,"        sudo iw dev wlan0 set channel 1\n");
+	fprintf(file,"        sudo iw wlan0 connect wifindus_mesh keys 0:PWbDq39QQ8632\n");
+	
+	
+	
 	fprintf(file,"        sudo ifconfig wlan0 down\n");
 	fprintf(file,"        sudo iwconfig wlan0 channel 1\n");
 	fprintf(file,"        sudo iwconfig wlan0 mode Ad-Hoc\n");
@@ -151,6 +159,7 @@ int write_rc_local(int num)
 	fprintf(file,"        sudo iwconfig wlan0 key s:PWbDq39QQ8632\n");
 	fprintf(file,"        sudo ifconfig wlan0 192.168.2.%d\n",num);
 	fprintf(file,"        sudo ifconfig wlan0 up\n");
+	*/
 	fprintf(file,"fi\n\n");
 	
 	/*
