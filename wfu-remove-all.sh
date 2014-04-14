15fd8560e17a0d617333dee87aa08e09a07800ad
Mark Gillard <marzer_@hotmail.com>
2014-04-15 07:44:42 +0930

    added screen clear

--- wfu-remove-all.sh
+++ wfu-remove-all.sh
@@ -6,6 +6,7 @@
 # Description:
 #   Removes all WFU stuff from a system.
 #===============================================================
+clear
 echo -e "${STYLE_TITLE}         WIFINDUS BRAIN UNINSTALLATION          ${STYLE_NONE}"
 echo -e "${STYLE_WARNING}This will completely remove WFU and servald from the system!${STYLE_NONE}\n"
 echo -e "${STYLE_ERROR}Are you absolutely sure? Press CTRL-C to abort. You have 10 SECONDS.${STYLE_NONE}\n"
