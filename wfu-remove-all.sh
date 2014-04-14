4cc4b692f6fa91b2554e0b32aa1d4c11d6ca3e39
Mark Gillard <marzer_@hotmail.com>
2014-04-15 07:41:14 +0930

    improved remove-all functionality

--- wfu-remove-all.sh
+++ wfu-remove-all.sh
@@ -4,12 +4,16 @@
 # Author: Mark Gillard
 # Target environment: Raspbian
 # Description:
-#   Removes all WFU scripts etc from a system.
+#   Removes all WFU stuff from a system.
 #===============================================================
-echo -e "${STYLE_HEADING}Removing wfu configs created by wfu-setup...${STYLE_NONE}"
-echo -e "  ${STYLE_SUCCESS}hosts, hostname and network/interfaces\nare not deleted by this script.!${STYLE_NONE}"
+echo -e "${STYLE_TITLE}         WIFINDUS BRAIN UNINSTALLATION          ${STYLE_NONE}"
+echo -e "${STYLE_WARNING}This will completely remove WFU and servald from the system!${STYLE_NONE}\n"
+echo -e "${STYLE_ERROR}Are you absolutely sure? Press CTRL-C to abort. You have 10 SECONDS.${STYLE_NONE}\n"
+sleep 10;
+echo -e "${STYLE_WARNING}Alright then, off we go! The unit will be rebooted when this has completed.${STYLE_NONE}\n"
 sudo wfu-setup -u
-sudo rm -rf /home/pi/src/wfu-tools
-sudo rm -f /usr/bin/wfu-*
+sudo find / -name "wfu*" | xargs sudo rm -rf 
 sudo find / -name "*serval*" | xargs sudo rm -rf 
-echo -e "  ${STYLE_SUCCESS}done!${STYLE_NONE}\n"
+echo -e "${STYLE_SUCCESS}Finished :)\n${STYLE_YELLOW}Thanks! The system will reboot in 5 seconds.${STYLE_NONE}"
+sleep 5
+sudo shutdown -r now > /dev/null
