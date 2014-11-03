7d206e0c5370b18e296ce87eef02042dd5e8cbf1
marzer <marzer_@hotmail.com>
2014-11-04 08:29:14 +1030

    test nat stuff

--- wfu-setup.c
+++ wfu-setup.c
@@ -278,9 +278,11 @@ int write_rc_local(int num)
 		{
 			fprintf(file,"iptables -t nat -A POSTROUTING -d 192.168.1.0/24 -j ACCEPT\n");
 			fprintf(file,"iptables -t nat -A POSTROUTING -d 0.0.0.0/0 -j MASQUERADE\n");
-			fprintf(file,"ip route add 0.0.0.0/0 via 192.168.1.254 dev eth0\n");
+			
 			fprintf(file,"iptables -t nat -A POSTROUTING -o eth0 -d 192.168.1.0/24 -j ACCEPT\n");
 			fprintf(file,"iptables -t nat -A POSTROUTING -o eth0 -d 0.0.0.0/0 -j MASQUERADE\n");
+			
+			fprintf(file,"ip route add 0.0.0.0/0 via 192.168.1.254 dev eth0\n");
 		}
 		else
 			fprintf(file,"ip route add 0.0.0.0/0 via 10.1.0.1 dev mesh0\n");
