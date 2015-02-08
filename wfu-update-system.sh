6fc7e7c495e7295a887d28afd44e00ed26e7da54
marzer <marzer_@hotmail.com>
2015-02-09 00:36:55 +1030

    gpsd-clients, log verbosity

--- wfu-update-system.sh
+++ wfu-update-system.sh
@@ -12,26 +12,27 @@ sudo mkdir -p /var/cache/apt/archives/partial
 sudo mkdir -p /var/lib/apt/list/partial
 
 echo -e "  ${STYLE_HEADING}updating apt-get list...${STYLE_NONE}"
-sudo apt-get -qq update > /dev/null
+sudo apt-get -y update
 
 echo -e "  ${STYLE_HEADING}upgrading existing packages...${STYLE_NONE}"
-sudo apt-get -qq upgrade > /dev/null
+sudo apt-get -y upgrade
 
 echo -e "  ${STYLE_HEADING}upgrading distro...${STYLE_NONE}"
-sudo apt-get -qq dist-upgrade > /dev/null
+sudo apt-get -y dist-upgrade
 
 echo -e "  ${STYLE_HEADING}installing apps required by WFU...${STYLE_NONE}"
-sudo apt-get -qq install build-essential haveged hostapd iw git autoconf gpsd libgps-dev secure-delete isc-dhcp-server > /dev/null
-sudo update-rc.d -f hostapd remove > /dev/null 2>&1
-sudo update-rc.d -f hostapd stop 80 0 1 2 3 4 5 6 . > /dev/null 2>&1
-sudo update-rc.d -f isc-dhcp-server remove > /dev/null 2>&1
-sudo update-rc.d -f isc-dhcp-server stop 80 0 1 2 3 4 5 6 . > /dev/null 2>&1
-sudo update-rc.d -f gpsd remove > /dev/null 2>&1
-sudo update-rc.d -f gpsd stop 80 0 1 2 3 4 5 6 . > /dev/null 2>&1
+sudo apt-get -y install build-essential haveged hostapd iw git autoconf gpsd \
+libgps-dev secure-delete isc-dhcp-server gpsd-clients
+sudo update-rc.d -f hostapd remove
+sudo update-rc.d -f hostapd stop 80 0 1 2 3 4 5 6 .
+sudo update-rc.d -f isc-dhcp-server remove
+sudo update-rc.d -f isc-dhcp-server stop 80 0 1 2 3 4 5 6 .
+sudo update-rc.d -f gpsd remove
+sudo update-rc.d -f gpsd stop 80 0 1 2 3 4 5 6 .
 
 echo -e "  ${STYLE_HEADING}cleaning up...${STYLE_NONE}"
-sudo apt-get -qq autoremove > /dev/null
-sudo apt-get -qq clean > /dev/null
-sudo apt-get -qq autoclean > /dev/null
+sudo apt-get -y autoremove
+sudo apt-get -y clean
+sudo apt-get -y autoclean
 
 echo -e "  ${STYLE_SUCCESS}done!${STYLE_NONE}\n"
