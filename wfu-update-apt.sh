452a0074bd93bb474ccfbdb9fcfb229974bbe0a7
Mark Gillard <marzer_@hotmail.com>
2015-03-17 17:53:28 +1030

    non-jessie on cubox

--- wfu-update-apt.sh
+++ wfu-update-apt.sh
@@ -26,9 +26,13 @@ if [ $IS_RASPBERRY_PI -eq 1 ]; then
 	sudo sh -c 'echo "deb http://mirrordirector.raspbian.org/raspbian/ wheezy main contrib non-free rpi" > /etc/apt/sources.list'
 	sudo sh -c 'echo "deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi" >> /etc/apt/sources.list'
 elif [ $IS_CUBOX -eq 1 ]; then
-	sudo sh -c 'echo "deb http://ftp.de.debian.org/debian wheezy main contrib non-free" > /etc/apt/sources.list'
+	sudo sh -c 'echo "deb http://ftp.au.debian.org/debian stable main contrib non-free" > /etc/apt/sources.list'
+	sudo sh -c 'echo "deb http://ftp.debian.org/debian/ wheezy-updates main contrib non-free" >> /etc/apt/sources.list'
+	sudo sh -c 'echo "deb http://security.debian.org/ wheezy/updates main contrib non-free" >> /etc/apt/sources.list'
+	#sudo sh -c 'echo "deb http://ftp.au.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list'
+	#sudo sh -c 'echo "deb http://ftp.debian.org/debian/ jessie-updates main contrib non-free" >> /etc/apt/sources.list'
+	#sudo sh -c 'echo "deb http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list'
 	sudo sh -c 'echo "deb http://repo.maltegrosse.de/debian/wheezy/bsp_cuboxi/ ./" >> /etc/apt/sources.list'
-	sudo sh -c 'echo "deb http://ftp.de.debian.org/debian jessie main contrib non-free" >> /etc/apt/sources.list'
 fi
 
 #===============================================================
@@ -38,9 +42,13 @@ fi
 sudo sh -c 'echo "Package: *" > /etc/apt/preferences'
 sudo sh -c 'echo "Pin: release n=wheezy" >> /etc/apt/preferences'
 sudo sh -c 'echo "Pin-Priority: 900" >> /etc/apt/preferences'
-sudo sh -c 'echo "Package: build-essential" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin: release n=jessie" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin-Priority: 910" >> /etc/apt/preferences'
+
+if [ $IS_RASPBERRY_PI -eq 1 ]; then
+	sudo sh -c 'echo "Package: build-essential" >> /etc/apt/preferences'
+	sudo sh -c 'echo "Pin: release n=jessie" >> /etc/apt/preferences'
+	sudo sh -c 'echo "Pin-Priority: 910" >> /etc/apt/preferences'
+fi
+
 sudo sh -c 'echo "Package: *" >> /etc/apt/preferences'
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
 	sudo sh -c 'echo "Pin: release o=Raspbian" >> /etc/apt/preferences'
