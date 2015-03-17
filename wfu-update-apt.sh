b2caa11f662ec3fcdfd33fb6d7d7e47c2b6d07ea
Mark Gillard <marzer_@hotmail.com>
2015-03-17 23:58:04 +1030

    removed jessie stuff

--- wfu-update-apt.sh
+++ wfu-update-apt.sh
@@ -24,14 +24,10 @@ fi
 
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
 	sudo sh -c 'echo "deb http://mirrordirector.raspbian.org/raspbian/ wheezy main contrib non-free rpi" > /etc/apt/sources.list'
-	sudo sh -c 'echo "deb http://mirrordirector.raspbian.org/raspbian/ jessie main contrib non-free rpi" >> /etc/apt/sources.list'
 elif [ $IS_CUBOX -eq 1 ]; then
 	sudo sh -c 'echo "deb http://ftp.au.debian.org/debian stable main contrib non-free" > /etc/apt/sources.list'
 	sudo sh -c 'echo "deb http://ftp.debian.org/debian/ wheezy-updates main contrib non-free" >> /etc/apt/sources.list'
 	sudo sh -c 'echo "deb http://security.debian.org/ wheezy/updates main contrib non-free" >> /etc/apt/sources.list'
-	sudo sh -c 'echo "deb http://ftp.au.debian.org/debian testing main contrib non-free" >> /etc/apt/sources.list'
-	sudo sh -c 'echo "deb http://ftp.debian.org/debian/ jessie-updates main contrib non-free" >> /etc/apt/sources.list'
-	sudo sh -c 'echo "deb http://security.debian.org/ jessie/updates main contrib non-free" >> /etc/apt/sources.list'
 	sudo sh -c 'echo "deb http://repo.maltegrosse.de/debian/wheezy/bsp_cuboxi/ ./" >> /etc/apt/sources.list'
 fi
 
@@ -39,14 +35,7 @@ fi
 # APT PREFERENCES
 #===============================================================
 
-sudo sh -c 'echo "Package: *" > /etc/apt/preferences'
-sudo sh -c 'echo "Pin: release a=wheezy" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin-Priority: 900" >> /etc/apt/preferences'
-
-sudo sh -c 'echo "Package: build-essential" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin: release a=jessie" >> /etc/apt/preferences'
-sudo sh -c 'echo "Pin-Priority: 910" >> /etc/apt/preferences'
-
+sudo rm -f /etc/apt/preferences
 
 #===============================================================
 # APT-UPDATE
