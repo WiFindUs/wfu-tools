257f990fbca3c9fefa1182675f7777891e7968b6
Mark Gillard <marzer_@hotmail.com>
2015-03-25 00:25:08 +1030

    forced root, added function, user creation

--- wfu-initial-setup.sh
+++ wfu-initial-setup.sh
@@ -16,6 +16,13 @@
 # ENVIRONMENT
 #===============================================================
 
+# root check
+if [[ $EUID -ne 0 ]]; then
+   echo "ERROR: wfu-initial-setup must be run as root!"
+   exit 2
+fi
+
+#shell globals
 if [ -f "/usr/local/wifindus/wfu-tools/wfu-shell-globals.sh" ]; then
 	source "/usr/local/wifindus/wfu-tools/wfu-shell-globals.sh"
 else
@@ -23,9 +30,26 @@ else
 	exit 1
 fi
 
-sudo chown "$CURRENT_USER" "$WFU_HOME"
-cd "$WFU_TOOLS"
-sudo chmod 777 *.sh
+#read number function
+read_number ()
+{
+	VALUE=""
+	while [ -z "$VALUE" ]
+	do
+		echo -n -e "  ${STYLE_PROMPT}Enter $1 ($2-$3):${STYLE_NONE} "
+		read VALUE
+
+		if [ -n "$VALUE" ]; then
+			while [ $VALUE -lt $2 ] || [ $VALUE -gt $3 ]
+			do
+				echo -e "    ${STYLE_ERROR}outside range!${STYLE_NONE}"
+				VALUE=""
+			done
+		fi
+	done
+
+	return $VALUE
+}
 
 #===============================================================
 # INTRO
@@ -33,14 +57,40 @@ sudo chmod 777 *.sh
 
 clear
 echo -e "${STYLE_TITLE}        WIFINDUS BRAIN #$WFU_BRAIN_ID_HEX INITIAL SETUP        ${STYLE_NONE}"
-echo -e "${STYLE_HEADING}Current user: ${STYLE_NONE}$CURRENT_USER ($CURRENT_HOME)\n"
 echo -e "${STYLE_HEADING}Machine model: ${STYLE_NONE}$MACHINE_MODEL\n"
 echo -e "${STYLE_WARNING}NOTE: The unit will be rebooted when this has completed.${STYLE_NONE}\n"
 echo -e "${STYLE_HEADING}Just a bit of information from you to start with...${STYLE_NONE}"
 read_number "this unit's ID #" 1 254
 WFU_BRAIN_NUM=$?
 export WFU_BRAIN_NUM
-PASSWORD=`read_password "a password for the user '$CURRENT_USER'" 6 12`
+
+
+#===============================================================
+# WIFINDUS USER ACCOUNT
+#===============================================================
+
+HAS_WFU_USER=`cat /etc/passwd | grep $WFU_USER`
+if [ -z "HAS_WFU_USER" ]; then
+	echo -e "${STYLE_HEADING}User '$WFU_USER' missing! creating...${STYLE_NONE}\n"
+	adduser "$WFU_USER"
+	echo "$WFU_USER ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+	if [ $IS_RASPBERRY_PI -eq 1 ]; then
+		HAS_PI_USER=`cat /etc/passwd | grep pi`
+		if [ -n "HAS_PI_USER" ]; then
+			echo -e "${STYLE_HEADING}Deleting default user 'pi'...${STYLE_NONE}\n"
+			deluser -remove-home pi
+		fi
+	fi
+fi
+
+#===============================================================
+# WIFINDUS DIRECTORY
+#===============================================================
+
+chown "$WFU_USER" "$WFU_HOME"
+cd "$WFU_TOOLS"
+chmod 777 *.sh
+
 echo -e "  ${STYLE_INFO}...that's all I need for now. The script will take a few minutes.${STYLE_NONE}\n"
 
 #===============================================================
@@ -55,7 +105,7 @@ echo -e "${STYLE_HEADING}Updating apt-get database...${STYLE_NONE}"
 #===============================================================
 
 echo -e "${STYLE_HEADING}Uninstalling unnecessary packages...${STYLE_NONE}"
-sudo apt-get -y purge xserver* x11-common x11-utils x11-xkb-utils  \
+apt-get -y purge xserver* x11-common x11-utils x11-xkb-utils  \
 	wpasupplicant wpagui scratch xpdf idle midori netsurf-common \
 	debian-reference* libpoppler19 x11-xserver-utils dillo \
 	xarchiver xauth xkb-data console-setup \
@@ -71,64 +121,63 @@ sudo apt-get -y purge xserver* x11-common x11-utils x11-xkb-utils  \
 	udhcpd xdg-utils libfreetype* bash-completion ncurses-term wpasupplicant \
 	vim-common vim-tiny
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
-	sudo apt-get -y purge omxplayer pistore wolfram-engine sonic-pi penguinspuzzle \
+	apt-get -y purge omxplayer pistore wolfram-engine sonic-pi penguinspuzzle \
 	^libraspberrypi-* raspberrypi-artwork
 fi 
 
 echo -e "${STYLE_HEADING}Removing config-only apt entries...${STYLE_NONE}"
-dpkg -l | grep -o -E "^rc  [a-zA-Z0-9\\.-]+" | grep -o -E "[a-zA-Z0-9\\.-]+$" | tr -s "\n" " " | xargs sudo apt-get -y purge
+dpkg -l | grep -o -E "^rc  [a-zA-Z0-9\\.-]+" | grep -o -E "[a-zA-Z0-9\\.-]+$" | tr -s "\n" " " | xargs apt-get -y purge
 
 echo -e "${STYLE_HEADING}Deleting GUI/junk files...${STYLE_NONE}"
-cd "$CURRENT_HOME"
-sudo rm -f ocr_pi.png
-sudo rm -f /lib/modules.bak
-sudo rm -rf /opt
-sudo rm -rf /usr/games/
-sudo rm -rf python_games
-sudo rm -rf indiecity
-sudo rm -rf /etc/X11
-sudo rm -rf /var/log/ConsoleKit
-sudo rm -rf /etc/polkit-1
-sudo rm -rf /usr/lib/xorg
-sudo rm -rf /usr/share/icons
-sudo rm -rf /usr/share/applications
-sudo rm -rf /etc/console-setup
-sudo rm -rf /usr/share/man/??
-sudo rm -rf /usr/share/man/??_*
-sudo rm -rf /usr/share/man/fr.*
+rm -f /lib/modules.bak
+rm -f /home/pi/ocr_pi.png
+rm -rf /opt
+rm -rf /usr/games/
+rm -rf /home/pi/python_games
+rm -rf /home/pi/indiecity
+rm -rf /etc/X11
+rm -rf /var/log/ConsoleKit
+rm -rf /etc/polkit-1
+rm -rf /usr/lib/xorg
+rm -rf /usr/share/icons
+rm -rf /usr/share/applications
+rm -rf /etc/console-setup
+rm -rf /usr/share/man/??
+rm -rf /usr/share/man/??_*
+rm -rf /usr/share/man/fr.*
 
 #===============================================================
 # UPDATE PACKAGES
 #===============================================================
 
 echo -e "${STYLE_HEADING}Updating remaining packages...${STYLE_NONE}"
-sudo apt-get -f -y install
-sudo apt-get -y upgrade
-sudo apt-get -y dist-upgrade
+apt-get -f -y install
+apt-get -y upgrade
+apt-get -y dist-upgrade
 
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
 	echo -e "${STYLE_HEADING}Updating Raspbian...${STYLE_NONE}"
-	sudo apt-get -y install rpi-update raspi-config
-	sudo apt-get -f -y install
-	sudo rpi-update
+	apt-get -y install rpi-update raspi-config
+	apt-get -f -y install
+	rpi-update
 fi
 
 echo -e "${STYLE_HEADING}Installing packages required by WFU...${STYLE_NONE}"
-sudo apt-get -y install build-essential haveged iw git autoconf gpsd \
+apt-get -y install build-essential haveged iw git autoconf gpsd \
 	secure-delete isc-dhcp-server gpsd-clients crda  \
 	firmware-ralink firmware-atheros ntp bc nano psmisc hostapd\
 	
-sudo update-rc.d -f hostapd remove
-sudo update-rc.d -f hostapd stop 80 0 1 2 3 4 5 6 .
-sudo update-rc.d -f isc-dhcp-server remove
-sudo update-rc.d -f isc-dhcp-server stop 80 0 1 2 3 4 5 6 .
-sudo update-rc.d -f gpsd remove
-sudo update-rc.d -f gpsd stop 80 0 1 2 3 4 5 6 .
+update-rc.d -f hostapd remove
+update-rc.d -f hostapd stop 80 0 1 2 3 4 5 6 .
+update-rc.d -f isc-dhcp-server remove
+update-rc.d -f isc-dhcp-server stop 80 0 1 2 3 4 5 6 .
+update-rc.d -f gpsd remove
+update-rc.d -f gpsd stop 80 0 1 2 3 4 5 6 .
 
 echo -e "${STYLE_HEADING}Cleaning up apt...${STYLE_NONE}"
-sudo apt-get -y autoremove
-sudo apt-get -y clean
-sudo apt-get -y autoclean
+apt-get -y autoremove
+apt-get -y clean
+apt-get -y autoclean
 
 #===============================================================
 # DOWNLOAD FIRMWARE AND BINARIES
@@ -137,7 +186,7 @@ sudo apt-get -y autoclean
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
 	if [ ! -f /lib/firmware/htc_9271.fw ]; then
 		echo -e "${STYLE_HEADING}Downloading Atheros 9271 firmware...${STYLE_NONE}"
-		sudo wget -O /lib/firmware/htc_9271.fw http://www.wifindus.com/downloads/htc_9271.fw
+		wget -O /lib/firmware/htc_9271.fw http://www.wifindus.com/downloads/htc_9271.fw
 		if [ ! -f /lib/firmware/htc_9271.fw ]; then
 			echo -e "  ${STYLE_ERROR}error! probably 404.${STYLE_NONE}"
 		fi
@@ -145,7 +194,7 @@ if [ $IS_RASPBERRY_PI -eq 1 ]; then
 
 	if [ ! -f /lib/firmware/htc_7010.fw ]; then
 		echo -e "${STYLE_HEADING}Downloading Atheros 7010 firmware...${STYLE_NONE}"
-		sudo wget -O /lib/firmware/htc_7010.fw http://www.wifindus.com/downloads/htc_7010.fw
+		wget -O /lib/firmware/htc_7010.fw http://www.wifindus.com/downloads/htc_7010.fw
 		if [ ! -f /lib/firmware/htc_7010.fw ]; then
 			echo -e "  ${STYLE_ERROR}error! probably 404.${STYLE_NONE}"
 		fi
@@ -166,35 +215,32 @@ cd "$WFU_TOOLS"
 
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
 	echo -e "${STYLE_HEADING}Disabling swap..${STYLE_NONE}"
-	sudo dphys-swapfile swapoff
-	sudo dphys-swapfile uninstall
-	sudo update-rc.d dphys-swapfile remove
-	sudo apt-get -y remove dphys-swapfile
+	dphys-swapfile swapoff
+	dphys-swapfile uninstall
+	update-rc.d dphys-swapfile remove
+	apt-get -y remove dphys-swapfile
 
 	echo -e "${STYLE_HEADING}Writing /boot/cmdline.txt...${STYLE_NONE}"
-	sudo sh -c 'echo "dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait smsc95xx.turbo_mode=N dwc_otg.microframe_schedule=1" > /boot/cmdline.txt'
+	echo "dwc_otg.lpm_enable=0 console=tty1 root=/dev/mmcblk0p2 rootfstype=ext4 elevator=deadline rootwait smsc95xx.turbo_mode=N dwc_otg.microframe_schedule=1" > /boot/cmdline.txt
 fi
 
 HAYSTACK=`cat "/etc/modules" | grep -o -m 1 -E "rt2800usb"`
 if [ ! -f "/etc/modules" ] || [ -z "$HAYSTACK" ]; then
 	echo -e "${STYLE_HEADING}Writing /etc/modules...${STYLE_NONE}"
-	sudo sh -c 'echo "rt2800usb" >> "/etc/modules"'
+	echo "rt2800usb" >> "/etc/modules"
 fi
 
 HAYSTACK=`cat /etc/sysctl.conf | grep -o -m 1 -E "net[.]ipv6[.]conf[.]all[.]disable_ipv6 *= *1"`
 if [ -z "$HAYSTACK" ]; then
 	echo -e "${STYLE_HEADING}Updating /etc/sysctl.conf...${STYLE_NONE}"
-	sudo sh -c 'echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf'
-	sudo sh -c 'echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf'
-	sudo sh -c 'echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf'
-	sudo sh -c 'echo "net.ipv6.conf.eth0.disable_ipv6 = 1" >> /etc/sysctl.conf'
+	echo "net.ipv6.conf.all.disable_ipv6 = 1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.default.disable_ipv6 = 1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.lo.disable_ipv6 = 1" >> /etc/sysctl.conf
+	echo "net.ipv6.conf.eth0.disable_ipv6 = 1" >> /etc/sysctl.conf
 fi
 
 echo -e "${STYLE_HEADING}Running wfu-setup...${STYLE_NONE}"
-sudo wfu-setup $WFU_BRAIN_NUM
-
-echo -e "\n${STYLE_HEADING}Setting Unix password for '$CURRENT_USER'...${STYLE_NONE}"
-echo -e "$PASSWORD\n$PASSWORD\n" | sudo passwd $CURRENT_USER
+wfu-setup $WFU_BRAIN_NUM
 
 #===============================================================
 # FINISH
@@ -202,11 +248,11 @@ echo -e "$PASSWORD\n$PASSWORD\n" | sudo passwd $CURRENT_USER
 
 if [ $IS_RASPBERRY_PI -eq 1 ]; then
 	echo -e "${STYLE_HEADING}Launching raspi-config...${STYLE_NONE}"
-	sudo raspi-config
+	raspi-config
 fi
 
 echo -e "${STYLE_SUCCESS}Finished :)\n${STYLE_YELLOW}The system will reboot in 5 seconds.${STYLE_NONE}"
 sleep 5
-sudo shutdown -r now
+reboot
 
 exit 0
