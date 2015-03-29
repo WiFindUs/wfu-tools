c15d4010e1f2d2670ce345dad370df44fb8ddaa9
Mark Gillard <marzer_@hotmail.com>
2015-03-30 09:56:47 +1030

    added a lot more info

--- wfu-brain-info.sh
+++ wfu-brain-info.sh
@@ -15,12 +15,76 @@ else
 	exit 1
 fi
 
-#node info
-MESSAGE="Brain environment information:"
-MESSAGE="$MESSAGE\n  Node version: $WFU_VERSION"
-MESSAGE="$MESSAGE\n  Node ID     : $WFU_BRAIN_ID_HEX"
-MESSAGE="$MESSAGE\n  Station #   : $WFU_BRAIN_NUM"
-MESSAGE="$MESSAGE\n  AP channel  : $WFU_AP_CHANNEL"
-echo -e $MESSAGE
+# collect data
+HOSTAPD=`sudo pgrep -l hostapd`
+if [ -n "$HOSTAPD" ]; then
+	HOSTAPD="running"
+else
+	HOSTAPD="not found"
+fi
+DHCPD=`sudo pgrep -l dhcpd`
+if [ -n "$DHCPD" ]; then
+	DHCPD="running"
+else
+	DHCPD="not found"
+fi
+GPSD=`sudo pgrep -l gpsd`
+if [ -n "$GPSD" ]; then
+	GPSD="running"
+else
+	GPSD="not found"
+fi
+MESH_0=`ifconfig | grep -m 1 "^mesh0"`
+if [ -n "$MESH_0" ]; then
+	MESH_0="mesh0"
+	MESH_PEERS=`sudo iw dev mesh0 mpath dump | grep mesh0`
+else
+	MESH_0="not found"
+fi
+AP_0=`ifconfig | grep -m 1 "^ap0"`
+if [ -n "$AP_0" ]; then
+	AP_0="ap0"
+else
+	AP_0="not fount"
+fi
+if [ $IS_RASPBERRY_PI -eq 1 ]; then
+	IS_RPI="yes"
+else
+	IS_RPI="no"
+fi
+
+if [ $IS_CUBOX -eq 1 ]; then
+	IS_CBX="yes"
+else
+	IS_CBX="no"
+fi
+
+if [ -n "$LAST_UPDATE_TIME" ]; then
+	LAST_UPDATE="$LAST_UPDATE_TIME"
+else
+	LAST_UPDATE="unknown"
+fi
+
+#print info
+echo "Brain environment information:"
+echo "  Node version  : $WFU_VERSION"
+echo "  Node ID       : $WFU_BRAIN_ID_HEX"
+echo "  Last updated  : $LAST_UPDATE"
+echo "  Station #     : $WFU_BRAIN_NUM"
+echo "  Machine model : $MACHINE_MODEL"
+echo "  Machine family: $MACHINE_FAMILY"
+echo "    - is Pi     : $IS_RPI"
+echo "    - is Cubox  : $IS_CBX"
+echo "  Mesh point    : $MESH_0"
+echo "  Access point  : $AP_0"
+echo "    - channel   : $WFU_AP_CHANNEL"
+echo "    - hostapd   : $HOSTAPD"
+echo "    - dhcpd     : $DHCPD"
+echo "  GPS daemon    : $GPSD"
+echo "  Mesh point    : $MESH_0"
+if [ -n "$MESH_PEERS" ]; then
+	echo "    - peers     :"
+	echo -e "$MESH_PEERS"
+fi
 
 exit 0
