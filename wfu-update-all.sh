26637412a4023b46cc7a2a45a4d41cc6feb5786e
Mark Gillard <marzer_@hotmail.com>
2015-04-01 12:23:12 +1030

    formatting, warnings, timing

--- wfu-update-all.sh
+++ wfu-update-all.sh
@@ -7,7 +7,10 @@
 #   Remotely updates and reboots the entire mesh.
 #===============================================================
 
-# environment
+#===============================================================
+# ENVIRONMENT
+#===============================================================
+
 if [ -f "/usr/local/wifindus/wfu-tools/wfu-shell-globals.sh" ]; then
 	source "/usr/local/wifindus/wfu-tools/wfu-shell-globals.sh"
 else
@@ -26,10 +29,48 @@ if [ -z "$MESH_0" ]; then
 	exit 3
 fi
 
-wfu-brain-send "wfu-update; sleep 15; sudo reboot"
+#===============================================================
+# INTRO
+#===============================================================
 
+clear
+echo -e "${STYLE_TITLE}                WIFINDUS MESH-WIDE UPDATE                  ${STYLE_NONE}"
+echo -e "${STYLE_INFO}This utility sends an update and reboot command to every visible${STYLE_NONE}"
+echo -e "${STYLE_INFO}node on the mesh, with sleep timers to allow for all updates to finish.${STYLE_NONE}"
+echo -e "${STYLE_WARNING}Significant mesh disruption will occur for a few minutes!${STYLE_NONE}\n"
+
+echo -n -e "  ${STYLE_PROMPT}Continue?${STYLE_NONE} "
+read -n 1 -r
+if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+   echo ""
+   exit 4
+fi
+
+#===============================================================
+# SEND COMMANDS TO NODES
+#===============================================================
+
+wfu-brain-send "wfu-update; sleep 30; sudo reboot"
+
+#===============================================================
+# SELF-UPDATE
+#===============================================================
+
+sleep 10
 wfu-update
-sleep 15
+
+#===============================================================
+# FINISH
+#===============================================================
+
+echo -e "${STYLE_SUCCESS}Finished :)\n${STYLE_YELLOW}The system will reboot in 30 seconds.${STYLE_NONE}"
+sleep 10
+echo -e "${STYLE_YELLOW}The system will reboot in 20 seconds.${STYLE_NONE}"
+sleep 10
+echo -e "${STYLE_YELLOW}The system will reboot in 10 seconds.${STYLE_NONE}"
+sleep 5
+echo -e "${STYLE_YELLOW}The system will reboot in 5 seconds.${STYLE_NONE}"
+sleep 5
 sudo reboot
 
 exit 0
